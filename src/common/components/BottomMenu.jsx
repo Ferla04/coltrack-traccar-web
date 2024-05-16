5ab163f7931990ccce94cd79ca1a56c0f3025210
Ferla04 <fer.velas135@gmail.com>
2024-05-16 18:49:16 -0500

    ❎ Quitar la funcionalidad de logout en el BottomMenu

--- src/common/components/BottomMenu.jsx
+++ src/common/components/BottomMenu.jsx
@@ -1,33 +1,27 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router-dom';
 import {
-  Paper, BottomNavigation, BottomNavigationAction, Menu, MenuItem, Typography, Badge,
+  Paper, BottomNavigation, BottomNavigationAction, Badge,
 } from '@mui/material';
 
 import DescriptionIcon from '@mui/icons-material/Description';
 import SettingsIcon from '@mui/icons-material/Settings';
 import MapIcon from '@mui/icons-material/Map';
 import PersonIcon from '@mui/icons-material/Person';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
-import { sessionActions } from '../../store';
 import { useTranslation } from './LocalizationProvider';
 import { useRestriction } from '../util/permissions';
-import { nativePostMessage } from './NativeInterface';
 
 const BottomMenu = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const dispatch = useDispatch();
   const t = useTranslation();
 
   const readonly = useRestriction('readonly');
   const disableReports = useRestriction('disableReports');
   const user = useSelector((state) => state.session.user);
   const socket = useSelector((state) => state.session.socket);
-
-  const [anchorEl, setAnchorEl] = useState(null);
 
   const currentSelection = () => {
     if (location.pathname === `/settings/user/${user.id}`) {
@@ -42,41 +36,7 @@ const BottomMenu = () => {
     return null;
   };
 
-  const handleAccount = () => {
-    setAnchorEl(null);
-    navigate(`/settings/user/${user.id}`);
-  };
-
-  const handleLogout = async () => {
-    setAnchorEl(null);
-
-    const notificationToken = window.localStorage.getItem('notificationToken');
-    if (notificationToken && !user.readonly) {
-      window.localStorage.removeItem('notificationToken');
-      const tokens = user.attributes.notificationTokens?.split(',') || [];
-      if (tokens.includes(notificationToken)) {
-        const updatedUser = {
-          ...user,
-          attributes: {
-            ...user.attributes,
-            notificationTokens: tokens.length > 1 ? tokens.filter((it) => it !== notificationToken).join(',') : undefined,
-          },
-        };
-        await fetch(`/api/users/${user.id}`, {
-          method: 'PUT',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(updatedUser),
-        });
-      }
-    }
-
-    await fetch('/api/session', { method: 'DELETE' });
-    nativePostMessage('logout');
-    navigate('/login');
-    dispatch(sessionActions.updateUser(null));
-  };
-
-  const handleSelection = (event, value) => {
+  const handleSelection = (_event, value) => {
     switch (value) {
       case 'map':
         navigate('/');
@@ -88,10 +48,7 @@ const BottomMenu = () => {
         navigate('/settings/preferences');
         break;
       case 'account':
-        setAnchorEl(event.currentTarget);
-        break;
-      case 'logout':
-        handleLogout();
+        navigate(`/settings/user/${user.id}`);
         break;
       default:
         break;
@@ -110,24 +67,18 @@ const BottomMenu = () => {
           )}
           value="map"
         />
+
         {!disableReports && (
           <BottomNavigationAction label={t('reportTitle')} icon={<DescriptionIcon />} value="reports" />
         )}
+
         <BottomNavigationAction label={t('settingsTitle')} icon={<SettingsIcon />} value="settings" />
-        {readonly ? (
-          <BottomNavigationAction label={t('loginLogout')} icon={<ExitToAppIcon />} value="logout" />
-        ) : (
+
+        {!readonly ? (
           <BottomNavigationAction label={t('settingsUser')} icon={<PersonIcon />} value="account" />
-        )}
+        ) : null}
+
       </BottomNavigation>
-      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={() => setAnchorEl(null)}>
-        <MenuItem onClick={handleAccount}>
-          <Typography color="textPrimary">{t('settingsUser')}</Typography>
-        </MenuItem>
-        <MenuItem onClick={handleLogout}>
-          <Typography color="error">{t('loginLogout')}</Typography>
-        </MenuItem>
-      </Menu>
     </Paper>
   );
 };
