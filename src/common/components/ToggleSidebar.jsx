980ddad3d4b7ecdc73c6b65a69aaa48be2624dfa
Ferla04 <fer.velas135@gmail.com>
2024-05-22 23:28:25 -0500

    ✨ Change button toogleSideBar color

--- src/common/components/ToggleSidebar.jsx
+++ src/common/components/ToggleSidebar.jsx
@@ -6,7 +6,7 @@ const ToggleSidebar = () => {
   const theme = useTheme();
 
   return (
-    <section className="toggle-sidebar" style={{ backgroundColor: theme.palette.primary.main }}>
+    <section className="toggle-sidebar" style={{ backgroundColor: theme.palette.background.main }}>
       <div>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
           <path d="M233.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L301.3 256 438.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
