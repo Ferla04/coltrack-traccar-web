05b6e696d01b19cf4a83a6ed1d9111d3c428be96
Ferla04 <fer.velas135@gmail.com>
2024-05-23 18:54:47 -0500

    ✨ Login styles

--- src/login/LoginLayout.jsx
+++ src/login/LoginLayout.jsx
@@ -1,26 +1,52 @@
 import React from 'react';
-import { useMediaQuery, Paper } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
-import { useTheme } from '@mui/material/styles';
-import LogoImage from './LogoImage';
+import Logo from '../resources/images/logo.png';
+import LoginImage from '../resources/images/login-image.png';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     height: '100%',
   },
-  sidebar: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    background: theme.palette.primary.main,
-    paddingBottom: theme.spacing(5),
-    width: theme.dimensions.sidebarWidth,
-    [theme.breakpoints.down('lg')]: {
-      width: theme.dimensions.sidebarWidthTablet,
+  triangle: {
+    position: 'fixed',
+    width: 0,
+    height: 0,
+    borderRight: '50vw solid transparent',
+    borderLeft: `50vw solid ${theme.palette.background.main}`,
+    borderBottom: '50vh solid transparent',
+    borderTop: `50vh solid ${theme.palette.background.main}`,
+    top: 0,
+    left: 0,
+    zIndex: -1,
+  },
+  imgLogo: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    marginTop: 30,
+    marginLeft: 30,
+    width: 230,
+    [theme.breakpoints.down('md')]: {
+      width: 200,
     },
     [theme.breakpoints.down('sm')]: {
-      width: '0px',
+      width: 170,
+    },
+  },
+  imgLoginImage: {
+    position: 'fixed',
+    bottom: 0,
+    right: 0,
+    width: 600,
+    [theme.breakpoints.down('lg')]: {
+      width: 400,
+    },
+    [theme.breakpoints.down('md')]: {
+      width: 350,
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: 230,
     },
   },
   paper: {
@@ -30,31 +56,31 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     flex: 1,
     boxShadow: '-2px 0px 16px rgba(0, 0, 0, 0.25)',
-    [theme.breakpoints.up('lg')]: {
-      padding: theme.spacing(0, 25, 0, 0),
-    },
+    backgroundColor: 'transparent',
   },
   form: {
     maxWidth: theme.spacing(52),
     padding: theme.spacing(5),
     width: '100%',
+    [theme.breakpoints.down('sm')]: {
+      width: '98%',
+    },
   },
 }));
 
 const LoginLayout = ({ children }) => {
   const classes = useStyles();
-  const theme = useTheme();
 
   return (
     <main className={classes.root}>
-      <div className={classes.sidebar}>
-        {!useMediaQuery(theme.breakpoints.down('lg')) && <LogoImage color={theme.palette.secondary.contrastText} />}
+      <div style={{ zIndex: -1 }}>
+        <div className={classes.triangle} />
+        <img src={Logo} alt="logo-coltrack" className={classes.imgLogo} />
+        <img src={LoginImage} alt="logo-coltrack" className={classes.imgLoginImage} />
       </div>
-      <Paper className={classes.paper}>
-        <form className={classes.form}>
-          {children}
-        </form>
-      </Paper>
+      <div className={classes.paper}>
+        <form className={classes.form}>{children}</form>
+      </div>
     </main>
   );
 };
